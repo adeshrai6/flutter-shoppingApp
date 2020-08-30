816885a9c8ef176aee775c35e08571da622a27bc
Adesh Rai <adeshrai@adeshs-mbp.home>
2020-08-30 13:20:33 -0400

    init

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext contex) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(<Widget>[Text('What would you like to find')]),
+      ),
+    );
+  }
+}
