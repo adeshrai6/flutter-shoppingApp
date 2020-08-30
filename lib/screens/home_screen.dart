dfd43ad8d4c39527f8ae9fd5f904146119ec4e6d
Adesh Rai <adeshrai@adeshs-mbp.home>
2020-08-30 13:22:30 -0400

    init

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,9 +9,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext contex) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(<Widget>[Text('What would you like to find')]),
-      ),
-    );
+        // body: SafeArea(
+        //   // child: ListView(<Widget>[Text('What would you like to find')]),
+        // ),
+        );
   }
 }
